49694f8c6735d77528a941ae0890694d6b780784
TesaJ <nabateesajuliet@gmail.com>
2019-02-20 14:02:11 +0300

    vowelss.py

---
+++ vowelss.py
@@ -0,0 +1,8 @@
+s="asdnjnoissioeuu"
+numVowels = 0 
+word = ""
+for char in s: if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
+ numVowels += 1 word+=str(char):
+# print("Number of vowels: " + str(numVowels)) 
+# print("Number of vowels: " + str(word)) 
+   print("('",str(word),"', ",numVowels,")")
