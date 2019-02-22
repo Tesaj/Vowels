e1bcb6eb607fbd3a10fc506e02bb84dcdd6fe05d
Juliet Nabateesa <nabateesajuliet@gmail.com>
2019-02-22 13:43:29 +0300

    second_comment

---
+++ fizz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(a, b):
+    a=[1,2,3]
+    b=[4,5,6]
+    y=len(a)+len(b)
+    z=y%3
+    p=y%5
+if z==0:
+    print(Fizz)
+elif p==0 :
+    print(Buzz)
+elif z==0 and p==0:
+    print(Fizzbuzz)
+fizzbuzz(a,b)
+
